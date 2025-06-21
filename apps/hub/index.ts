9706cf4ce8396bfbc53d3c75dc16a69f67a8f48e
snehendu098 <roysnehendupersonal@gmail.com>
2025-06-21 18:18:41 +0530

    ws client done

---
+++ apps/hub/index.ts
@@ -0,0 +1,131 @@
+import { randomUUIDv7, type ServerWebSocket } from "bun";
+import type { IncomingMessage, SignupIncomingMessage } from "common/types";
+import { prismaClient } from "db/client";
+
+const availableValidators: { validatorId: string, socket: ServerWebSocket<unknown>, publicKey: string }[] = [];
+
+const CALLBACKS : { [callbackId: string]: (data: IncomingMessage) => void } = {}
+const COST_PER_VALIDATION = 100; // in lamports
+
+Bun.serve({
+    fetch(req, server) {
+      if (server.upgrade(req)) {
+        return;
+      }
+      return new Response("Upgrade failed", { status: 500 });
+    },
+    port: 8081,
+    websocket: {
+        async message(ws: ServerWebSocket<unknown>, message: string) {
+            const data: IncomingMessage = JSON.parse(message);
+            
+            if (data.type === 'signup') {
+
+                    await signupHandler(ws, data.data);
+                
+            } else if (data.type === 'validate') {
+                CALLBACKS[data.data.callbackId](data);
+                delete CALLBACKS[data.data.callbackId];
+            }
+        },
+        async close(ws: ServerWebSocket<unknown>) {
+            availableValidators.splice(availableValidators.findIndex(v => v.socket === ws), 1);
+        }
+    },
+});
+
+async function signupHandler(ws: ServerWebSocket<unknown>, { ip, publicKey, callbackId }: SignupIncomingMessage) {
+    const validatorDb = await prismaClient.validator.findFirst({
+        where: {
+            publicKey,
+        },
+    });
+
+    if (validatorDb) {
+        ws.send(JSON.stringify({
+            type: 'signup',
+            data: {
+                validatorId: validatorDb.id,
+                callbackId,
+            },
+        }));
+
+        availableValidators.push({
+            validatorId: validatorDb.id,
+            socket: ws,
+            publicKey: validatorDb.publicKey,
+        });
+        return;
+    }
+    
+    //TODO: Given the ip, return the location
+    const validator = await prismaClient.validator.create({
+        data: {
+            ip,
+            publicKey,
+            location: 'unknown',
+        },
+    });
+
+    ws.send(JSON.stringify({
+        type: 'signup',
+        data: {
+            validatorId: validator.id,
+            callbackId,
+        },
+    }));
+
+    availableValidators.push({
+        validatorId: validator.id,
+        socket: ws,
+        publicKey: validator.publicKey,
+    });
+}
+
+setInterval(async () => {
+    const websitesToMonitor = await prismaClient.website.findMany({
+        where: {
+            disabled: false,
+        },
+    });
+
+    for (const website of websitesToMonitor) {
+        availableValidators.forEach(validator => {
+            const callbackId = randomUUIDv7();
+            console.log(`Sending validate to ${validator.validatorId} ${website.url}`);
+            validator.socket.send(JSON.stringify({
+                type: 'validate',
+                data: {
+                    url: website.url,
+                    callbackId
+                },
+            }));
+
+            CALLBACKS[callbackId] = async (data: IncomingMessage) => {
+                if (data.type === 'validate') {
+                    const { validatorId, status, latency, statusCode } = data.data;
+
+                    await prismaClient.$transaction(async (tx:any) => {
+                        await tx.websiteTick.create({
+                            data: {
+                                websiteId: website.id,
+                                validatorId,
+                                status,
+                                latency,
+                                createdAt: new Date(),
+                                statusCode: statusCode
+                            },
+                        });
+
+                        await tx.validator.update({
+                            where: { id: validatorId },
+                            data: {
+                                pendingPayouts: { increment: COST_PER_VALIDATION },
+                            },
+                        });
+                    });
+                }
+            };
+        });
+    }
+}, 60 * 1000);
---
+++ apps/validator/index.ts
@@ -0,0 +1,86 @@
+import { randomUUIDv7 } from "bun";
+import type { OutgoingMessage, SignupOutgoingMessage, ValidateOutgoingMessage } from "common/types";
+
+const CALLBACKS: {[callbackId: string]: (data: SignupOutgoingMessage) => void} = {}
+
+let validatorId: string | null = null;
+
+async function main() {
+    const publicKey = "your-public-key-here"; // Replace with your actual public key
+    const ws = new WebSocket("ws://localhost:8081");
+
+    ws.onmessage = async (event) => {
+        const data: OutgoingMessage = JSON.parse(event.data);
+        if (data.type === 'signup') {
+            CALLBACKS[data.data.callbackId]?.(data.data)
+            delete CALLBACKS[data.data.callbackId];
+        } else if (data.type === 'validate') {
+            await validateHandler(ws, data.data);
+        }
+    }
+
+    ws.onopen = async () => {
+        const callbackId = randomUUIDv7();
+        CALLBACKS[callbackId] = (data: SignupOutgoingMessage) => {
+            validatorId = data.validatorId;
+        }
+        
+
+        ws.send(JSON.stringify({
+            type: 'signup',
+            data: {
+                callbackId,
+                ip: '127.0.0.1',
+                publicKey: publicKey,
+                location: "US"
+            },
+        }));
+    }
+}
+
+async function validateHandler(ws: WebSocket, { url, callbackId, websiteId }: ValidateOutgoingMessage) {
+    console.log(`Validating ${url}`);
+    const startTime = Date.now();
+    
+
+    try {
+        const response = await fetch(url);
+        const endTime = Date.now();
+        const latency = endTime - startTime;
+        const status = response.status;
+
+        console.log(url);
+        console.log(status);
+        ws.send(JSON.stringify({
+            type: 'validate',
+            data: {
+                callbackId,
+                status: (status >= 200 && status < 400) ? 'Good' : 'Bad',
+                latency,
+                websiteId,
+                validatorId,
+                
+                statusCode: response.status
+            },
+        }));
+    } catch (error) {
+        ws.send(JSON.stringify({
+            type: 'validate',
+            data: {
+                callbackId,
+                status:'Bad',
+                latency: 1000,
+                websiteId,
+                validatorId,
+                
+            },
+        }));
+        console.error(error);
+    }
+}
+
+main();
+
+setInterval(async () => {
+
+}, 10000);
